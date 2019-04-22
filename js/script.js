23facc1a74580717b45e9303ea38f73eb66c7117
austinestrobel <austinestrobel@users.noreply.github.com>
2019-04-22 18:31:05 +0200

    major update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(function (){
+$(function () {
     var $orders = $('#orders');
     var $name = $('#name');
     var $drink = $('#drink');
@@ -13,80 +13,80 @@ $(function (){
     }
 
 
-//Using JSON for example, could use given API
+    //Using JSON for example, could use given API
     $.ajax({
         type: 'GET',
         url: 'http://rest.learncode.academy/api/Austin/Coffee/',
-        success: function(orders) {
-            $.each(orders, function(i, order) {
+        success: function (orders) {
+            $.each(orders, function (i, order) {
                 addOrder(order);
             });
         },
-        error: function() {
+        error: function () {
             alert('error loading orders');
         }
     });
 
-    $enter.on('click', function() {
+    $enter.on('click', function () {
         var order = {
             name: $name.val(),
             drink: $drink.val(),
-        }; 
+        };
 
         $.ajax({
             type: 'POST',
             url: 'http://rest.learncode.academy/api/Austin/Coffee/',
             data: order,
-            success: function(newOrder) {
+            success: function (newOrder) {
                 addOrder(newOrder);
-            }, 
-            error: function() {
+            },
+            error: function () {
                 alert('error saving order');
             }
         });
-    
+
         $name.val('');
         $drink.val('');
     });
 
-    $name.keypress(function(e) {
-        if(e.which == 13) {
+    $name.keypress(function (e) {
+        if (e.which == 13) {
             $enter.click();
         }
     });
 
-    $drink.keypress(function(e) {
-        if(e.which == 13) {
+    $drink.keypress(function (e) {
+        if (e.which == 13) {
             $enter.click();
         }
     });
 
-    $orders.delegate('.remove', 'click', function() {
-//cannot use this inside of a success function!!
+    $orders.delegate('.remove', 'click', function () {
+        //cannot use this inside of a success function!!
         var $li = $(this).closest('li');
         $.ajax({
             type: 'DELETE',
             url: 'http://rest.learncode.academy/api/Austin/Coffee/' + $(this).attr('data-id'),
-            success: function() {
-                $li.fadeOut(300, function() {
+            success: function () {
+                $li.fadeOut(300, function () {
                     $(this).remove();
                 });
             }
         });
     });
 
-    $orders.delegate('.editOrder', 'click', function() {
+    $orders.delegate('.editOrder', 'click', function () {
         var $li = $(this).closest('li');
         $li.find('input.name').val($li.find('span.name').html());
         $li.find('input.drink').val($li.find('span.drink').html());
         $li.addClass('edit');
     });
 
-    $orders.delegate('.cancelEdit', 'click', function() {
+    $orders.delegate('.cancelEdit', 'click', function () {
         $(this).closest('li').removeClass('edit');
     });
 
-    $orders.delegate('.saveEdit', 'click', function() {
+    $orders.delegate('.saveEdit', 'click', function () {
         var $li = $(this).closest('li');
         var order = {
             name: $li.find('input.name').val(),
@@ -95,18 +95,34 @@ $(function (){
 
         $.ajax({
             type: 'PUT',
-            url: 'http://rest.learncode.academy/api/Austin/Coffee/' +$li.attr('data-id'),
+            url: 'http://rest.learncode.academy/api/Austin/Coffee/' + $li.attr('data-id'),
             data: order,
-            success: function(newOrder) {
+            success: function (newOrder) {
                 $li.find('span.name').html(order.name);
                 $li.find('span.drink').html(order.drink);
                 $li.removeClass('edit');
-            }, 
-            error: function() {
+            },
+            error: function () {
                 alert('error updating order');
             }
         });
     });
 
-    
+
+
+    // Checkout
+
+    var $checkout = $('#checkoutButton');
+    var $total = $('#total');
+
+    $checkout.on('click', function () {
+        if ($orders.length >= 1) {
+            $total.append(`Your cart has ${$orders.length} item(s)`),
+                $total.append(`Your total is ${$orders.length * 5}`)
+        } else {
+            $total.append('There are no items in your cart')
+        }
+    })
+
+
 });
